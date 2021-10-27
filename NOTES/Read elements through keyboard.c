74637ee5b2a54fc7ca8a9329f4b653f0c87049cc
MadhunadhGurram <84019306+MadhunadhGurram@users.noreply.github.com>
2021-10-27 02:12:31 -0700

    Create Read elements through keyboard.c

---
+++ NOTES/Read elements through keyboard.c
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	int rno;
+	char name[20];
+	clrscr();
+	cout<<"enter rollnumber:";
+	cin>>rno;
+	cout<<"enter your name:";
+	cin>>name;
+	cout<<"rollnumber="<<rno;
+	cout<<"name="<<name;
+	getch();
+}
